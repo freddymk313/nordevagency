171ffeefa8c5e7ecb4c1a28e8e248a2b6e26205a
Freddymk <freddymk@MacBook-Air-de-INT.local>
2025-11-13 10:29:30 -0800

    test

--- components/Header.tsx
+++ components/Header.tsx
@@ -150,6 +150,7 @@ export default function Header() {
                   strokeLinejoin="round"
                   strokeWidth={2}
                   d="M13 7l5 5m0 0l-5 5m5-5H6"
+                  
                 />
               </svg>
             </button>
