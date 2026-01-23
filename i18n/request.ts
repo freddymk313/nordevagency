81947916311236b9a2cdd6d14029ab5070bf9d12
Freddymk <freddymk@Chrinovics-MacBook-Air.local>
2026-01-23 10:10:17 +0200

    file added req

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,11 +1,14 @@
-import {getRequestConfig} from 'next-intl/server';
- 
+import { cookies } from "next/headers";
+import { getRequestConfig } from "next-intl/server";
+
 export default getRequestConfig(async () => {
   // Static for now, we'll change this later
-  const locale = 'en';
- 
+  //   const locale = 'en';
+  const store = await cookies();
+  const locale = store.get("locale")?.value || "en";
+
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages: (await import(`../../messages/${locale}.json`)).default,
   };
 });
