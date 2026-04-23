76632abf53e6d1e8aeb97b345b04403a8968fee5
Freddymk <20mm313@esisalama.org>
2026-04-23 13:23:40 +0200

    trusted ok

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { motion, type Variants } from "framer-motion";
 import { ArrowDown, ArrowRight, Star } from "lucide-react";
+import Link from "next/link";
 
 const lineParent: Variants = {
   hidden: {},
@@ -82,20 +83,44 @@ export default function Hero() {
           className="mt-10 flex flex-col items-center justify-center gap-3 sm:flex-row sm:gap-4"
         >
           {/* Bouton principal avec ton Vert et son animation de Glow */}
-          <a
+          {/* <a
             href="#contact"
             className="group animate-pulse-glow-green font-display inline-flex items-center justify-center gap-2 rounded-lg bg-green-accent px-7 py-3.5 text-base font-bold text-black transition-transform hover:scale-105"
           >
-            Start Your Project
+            
             <ArrowRight className="h-4 w-4 transition-transform group-hover:translate-x-1" />
-          </a>
+          </a> */}
+
+           <Link href="#contact">
+              <button 
+              // className="bg- text-sm text-white pl-8 pr-2 py-2 rounded-full hover:bg-black transition-all duration-300 font-semibold shadow-lg hover:shadow-xl transform hover:-translate-y-1  group"
+              className="*animate-pulse-glow-green font-display flex items-center gap-4 justify-center rounded-full bg-green-accent pl-6 py-2 md:py-2.5 pr-3.5 text-sm font-bold text-black transition-transform *hover:scale-105 active:scale-95"
+              >
+                <span className="">Start Your Project</span>
+                <span className="bg-white w-10 h-10 rounded-full flex items-center justify-center text-black group-hover:bg-gray-100 transition-colors">
+                  <svg
+                    className="w-5 h-5 group-hover:translate-x-0.5 transition-transform"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M13 7l5 5m0 0l-5 5m5-5H6"
+                    />
+                  </svg>
+                </span>
+              </button>
+            </Link>
           
-          <a
+          <Link
             href="#portfolio"
-            className="inline-flex items-center justify-center rounded-full border border-border bg-transparent px-7 py-3.5 text-base font-semibold text-foreground transition-colors hover:bg-secondary"
+            className="inline-flex items-center justify-center rounded-full border border-border bg-transparent px-7 py-3.5 text-base font-semibold text-foreground transition-colors hover:bg-background"
           >
             See Our Work
-          </a>
+          </Link>
         </motion.div>
 
         <motion.div
